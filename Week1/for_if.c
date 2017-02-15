37c004c6321b1b951404c5ebfd3f860249025bdf
William Oberndorfer <will.oberndorfer@gmail.com>
2017-02-15 14:55:38 -0500

    add for_if

---
+++ Week1/for_if.c
@@ -0,0 +1,33 @@
+/* example looping program */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int i;
+
+    char ch = 'A';
+
+    char AB[]="This is my char array string\n";
+    printf("Char array %s\n", AB);
+
+    printf("Char = %c\n", ch);
+    /* printf("Char value = 0x%x\n", ch); */
+    /* printf("Char value = 0%d\n", ch); */
+
+    for (i = 0; i < 25; i++)
+    {
+        ch++;
+
+        if (i<12) {
+            printf("Char = %c\n", ch);
+          //  printf("Char value = 0x%x\n", ch);
+        }
+        else {
+            printf("Char = %c\n", ch+0x20);
+            /* printf("Char value = 0x%x\n", ch+0x20); */
+
+        }
+    }
+    return 0;
+}
